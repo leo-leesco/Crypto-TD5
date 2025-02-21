8e4f801e1e6431de7a760290fff1116d509990d0
Léo LEESCO <leo.leesco@gmail.com>
2025-02-21 05:07:51 +0100

    fix misspelled variable

--- src/bin/sign.rs
+++ src/bin/sign.rs
@@ -12,8 +12,8 @@ use TD5::{
     Q,
 };
 
-fn sign(pk: [u8; 32], message: &[u8]) -> (U256, U256) {
-    let (x, secret) = secret_expand(pk);
+fn sign(sk: [u8; 32], message: &[u8]) -> (U256, U256) {
+    let (x, secret) = secret_expand(sk);
 
     let secret = secret.to_little_endian();
     let mut prefix_message: Vec<u8> = Vec::with_capacity(secret.len() + message.len());
@@ -37,13 +37,13 @@ fn sign(pk: [u8; 32], message: &[u8]) -> (U256, U256) {
 
 fn main() {
     let mut args = std::env::args();
-    let pk: [u8; 32] = read(
+    let sk: [u8; 32] = read(
         args.nth(1)
-            .expect("Please provide the path to the private key"),
+            .expect("Please provide the path to the secret key"),
     )
-    .expect("Could not read the private key file")
+    .expect("Could not read the secret key file")
     .try_into()
-    .expect("Could not parse the private key as a 32-byte key");
+    .expect("Could not parse the secret key as a 32-byte key");
 
     let message = read(
         args.nth(2)
@@ -51,7 +51,7 @@ fn main() {
     )
     .expect("Could not read the message file");
 
-    let (rs, s) = sign(pk, &message);
+    let (rs, s) = sign(sk, &message);
     let rs = rs.to_little_endian();
     let s = s.to_little_endian();
 
