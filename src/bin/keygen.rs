f2247c6f7d4a005b549084243df4eaf2bfd065ea
Léo LEESCO <leo.leesco@gmail.com>
2025-02-21 03:55:28 +0100

    keygen

--- src/bin/keygen.rs
+++ src/bin/keygen.rs
@@ -1,24 +1,28 @@
+use std::fs::write;
+
 use primitive_types::U256;
-use TD4::{
-    conversion::{clamp, sanitize},
-    montgomery::Point,
-};
+use rand::Rng;
+use sha2::{Digest, Sha512};
+use TD5::point::G;
+
+fn secret_expand(secret: [u8; 32]) -> (U256, U256) {
+    let mut hasher = Sha512::new();
+    hasher.update(secret);
+    let hash = hasher.finalize();
+    let mut a = U256::from_little_endian(&hash[..32]);
+    a &= (U256::one() << 254) - U256([8, 0, 0, 0]);
+    a |= U256::one() << 254;
+    (a, U256::from_little_endian(&hash[32..]))
+}
+
+fn secret_to_public(secret: [u8; 32]) -> U256 {
+    let (a, _) = secret_expand(secret);
+    (G * a).compress()
+}
 
 fn main() {
-    let mut args = std::env::args();
-    let m = clamp(
-        U256::from_str_radix(&args.nth(1).expect("Please provide m"), 16)
-            .expect("Could not parse m as a 64-character hexadecimal string"),
-    );
-    let u = sanitize({
-        if let Some(u) = &args.nth(2) {
-            // DH key exchange
-            U256::from_str_radix(u, 16)
-                .expect("Could not parse m as a 64-character hexadecimal string")
-        } else {
-            // DH public key generation
-            U256([9, 0, 0, 0])
-        }
-    });
-    println!("{:0>64X}", (Point::new(u) * m).get());
+    let secret = rand::rng().random();
+    write("prefix.sk", secret).expect("Could not write the secret key to prefix.sk");
+    write("prefix.pk", secret_to_public(secret).to_little_endian())
+        .expect("Could not write the secret key to prefix.pk");
 }
